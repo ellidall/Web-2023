36de149712708a33d5a519f451aab1dcc67026a6
Alexander Apakaev <sanya.apakaev@mail.run>
2023-04-14 08:54:46 +0300

    LW6 14.04.23

--- cmd/blog/handlers.go
+++ cmd/blog/handlers.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type indexPage struct {
@@ -12,122 +14,108 @@ type indexPage struct {
 }
 
 type featuredPostData struct {
-	ImgModifier    string
-	ButtonModifier string
-	ButtonText     string	
-	Title          string
-	Subtitle       string
-	AuthorImg      string
-	Author         string
-	PublishDate    string
+	ImgModifier    string `db:"image_url"`
+	Title          string `db:"title"`
+	Subtitle       string `db:"subtitle"`
+	AuthorImg      string `db:"author_url"`
+	Author         string `db:"author"`
+	PublishDate    string `db:"publish_date"`
 }
 
 type mostRecentPostData struct {
-	PostImg     string
-	Title       string
-	Subtitle    string
-	AuthorImg   string
-	Author      string
-	PublishDate string
+	PostImg     string `db:"image_url"`
+	Title       string `db:"title"`
+	Subtitle    string `db:"subtitle"`
+	AuthorImg   string `db:"author_url"`
+	Author      string `db:"author"`
+	PublishDate string `db:"publish_date"`
 }
  
 
-func index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
-		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
-		return                                      // Не забываем завершить выполнение ф-ии
-	}
+func index(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		featuredPostsData, err := featuredPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return
+		}
 
-	data := indexPage{
-		FeaturedPosts:   featuredPosts(),
-		MostRecentPosts: mostRecentPosts(),
-	}
+		mostRecentPostsData, err := mostRecentPosts(db)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return
+		}
 
-	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
-	if err != nil {
-		http.Error(w, "Internal Server Error", 500)
-		log.Println(err.Error())
-		return
+		ts, err := template.ParseFiles("pages/index.html")
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return 
+		}
+
+		data := indexPage{
+			FeaturedPosts:   featuredPostsData,
+			MostRecentPosts: mostRecentPostsData,
+		}
+
+		err = ts.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Internal Server Error", 500)
+			log.Println(err)
+			return
+		}
+
+		log.Println("Request completed successfully")
 	}
 }
 
-func featuredPosts() []featuredPostData {
-	return []featuredPostData{
-		{
-			ImgModifier:    "featured-post__img_the-road-ahead",
-			ButtonModifier: "featured-post__button_none",
-			ButtonText:     "",
-			Title:          "The Road Ahead",
-			Subtitle:       "The road ahead might be paved - it might not be.",
-			AuthorImg:      "../static/img/face1.png",
-			Author:         "Mat Vogels",
-			PublishDate:    "September 25, 2015",
-		},
-		{
-			ImgModifier:    "featured-post__img_from-top-down",
-			ButtonModifier: "featured-post__button",
-			ButtonText:     "Adventure",
-			Title:          "From Top Down",
-			Subtitle:       "Once a year, go someplace you've never been before.",
-			AuthorImg:      "../static/img/face2.png",
-			Author:         "William Wong",
-			PublishDate:    "September 25, 2015",
-		},
+func featuredPosts(db *sqlx.DB) ([]featuredPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author, 
+			author_url, 
+			publish_date, 
+			image_url
+		FROM
+			post
+		WHERE featured = 1
+	`
+
+	var posts []featuredPostData
+
+	err := db.Select(&posts, query)
+	if err != nil {          
+		return nil, err
 	}
+
+	return posts, nil
 }
 
  
-func mostRecentPosts() []mostRecentPostData {
-	return []mostRecentPostData{
-		{
-			PostImg:     "../static/img/image3.jpg",
-			Title:       "Still Standing Tall",
-			Subtitle:    "Life begins at the end of your comfort zone.",
-			AuthorImg:   "../static/img/face2.png",
-			Author:      "William Wong",
-			PublishDate: "9/25/2015",
-		},
-		{
-			PostImg:     "../static/img/image4.png",
-			Title:       "Sunny Side Up",
-			Subtitle:    "No place is ever as bad as they tell you it's going to be.",
-			AuthorImg:   "../static/img/face1.png",
-			Author:      "Mat Vogels",
-			PublishDate: "9/25/2015",
-		},
-		{
-			PostImg:     "../static/img/image5.png",
-			Title:       "Water Falls",
-			Subtitle:    "We travel not to escape life, but for life not to escape us.",
-			AuthorImg:   "../static/img/face1.png",
-			Author:      "Mat Vogels",
-			PublishDate: "9/25/2015",
-		},
-		{
-			PostImg:     "../static/img/image6.png",
-			Title:       "Through the Mist",
-			Subtitle:    "Travel makes you see what a tiny place you occupy in the world.",
-			AuthorImg:   "../static/img/face2.png",
-			Author:      "William Wong",
-			PublishDate: "9/25/2015",
-		},
-		{
-			PostImg:     "../static/img/image7.png",
-			Title:       "Awaken Early",
-			Subtitle:    "Not all those who wander are lost.",
-			AuthorImg:   "../static/img/face1.png",
-			Author:      "Mat Vogels",
-			PublishDate: "9/25/2015",
-		},
-		{
-			PostImg:     "../static/img/image8.png",
-			Title:       "Try it Always",
-			Subtitle:    "The world is a book, and those who do not travel read only one page.",
-			AuthorImg:   "../static/img/face1.png",
-			Author:      "Mat Vogels",
-			PublishDate: "9/25/2015",
-		},
-	}	
+func mostRecentPosts(db *sqlx.DB) ([]mostRecentPostData, error) {
+	const query = `
+		SELECT
+			title,
+			subtitle,
+			author, 
+			author_url, 
+			publish_date, 
+			image_url
+		FROM
+			post
+		WHERE featured = 0
+	`
+
+	var posts []mostRecentPostData
+
+	err := db.Select(&posts, query)
+	if err != nil {          
+		return nil, err
+	}
+
+	return posts, nil	
 }
---
+++ data/sql/posts/posts.sql
@@ -0,0 +1,163 @@
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'The Road Ahead',
+        'The road ahead might be paved - it might not be.',
+        'Mat Vogels',
+        'static/img/face1.png',
+        '9/25/2015',
+        'featured-post__img_the-road-ahead',
+        1
+    );
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'From Top Down',
+        "Once a year, go someplace you've never been before.",
+        'William Wong',
+        'static/img/face2.png',
+        '9/25/2015',
+        'featured-post__img_from-top-down',
+        1
+    );
+
+
+
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Still Standing Tall',
+        'Life begins at the end of your comfort zone.',
+        'William Wong',
+        'static/img/face2.png',
+        '9/25/2015',
+        'static/img/image3.jpg',
+        0
+    );
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Sunny Side Up',
+        "No place is ever as bad as they tell you it's going to be.",
+        'Mat Vogels',
+        'static/img/face1.png',
+        '9/25/2015',
+        'static/img/image4.png',
+        0
+    );    
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Water Falls',
+        'We travel not to escape life, but for life not to escape us.',
+        'Mat Vogels',
+        'static/img/face1.png',
+        '9/25/2015',
+        'static/img/image5.png',
+        0
+    );   
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Through the Mist',
+        'Travel makes you see what a tiny place you occupy in the world.',
+        'William Wong',
+        'static/img/face2.png',
+        '9/25/2015',
+        'static/img/image6.png',
+        0
+    ); 
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Awaken Early',
+        'Not all those who wander are lost.',
+        'Mat Vogels',
+        'static/img/face1.png',
+        '9/25/2015',
+        'static/img/image7.png',
+        0
+    ); 
+INSERT INTO
+    post (
+        title,
+        subtitle,
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured
+    )
+VALUES
+    (
+        'Try it Always',
+        'The world is a book, and those who do not travel read only one page.',
+        'Mat Vogels',
+        'static/img/face1.png',
+        '9/25/2015',
+        'static/img/image8.png',
+        0
+    );                  
